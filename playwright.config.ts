769d72e7290b5f138511aefe46d0b60e8605a2db
GauthamBhe <161290308+GauthamBhe@users.noreply.github.com>
2026-04-25 23:07:23 +0530

    fixed report and removed allure from config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,13 +30,8 @@ const config: PlaywrightTestConfig = {
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-        // ['line'],
-        ['allure-playwright'],
-        // ['junit', { outputFile: 'results1.xml' }],
-        ['blob', { open: 'never' }],
-        ['json', { outputFile: 'test-results.json' }],
-        [process.env.CI === 'true' ? 'blob' : 'html', { open: 'never' }],
-    ],
+  ['html', { outputFolder: 'playwright-report', open: 'never' }]
+],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
